c262f9ecffff43ba12a9c9b7471cc5e5d0b96b57
LL-Program <lukas10.rennhofer@gmail.com>
2025-03-17 20:56:45 +0100

    Added CHIFEngine.h file for external Uses

--- src/CHIFEngine.h
+++ src/CHIFEngine.h
@@ -13,5 +13,37 @@
  * Last Change: Added Nintendo Switch (Homebrew) Files for Init
 */
 
-// TODO: CHIFEngine header with all components and namespaces (nested), for building games with low-level API
 // TODO: CMake file for compiling the CHIFEngine Library
+
+#ifndef CHIFENGINE
+#define CHIFENGINE
+#define CHIF_ENGINE
+
+#include <camera.h>
+#include <mesh.h>
+#include <model.h>
+
+#include "core/version.h"
+
+#include "Engine/BaseShader.h"
+#include "Engine/buffers.h"
+#include "Engine/sceneElements.h"
+#include "Engine/ScreenSpaceShader.h"
+#include "Engine/shader.h"
+
+#include "graphics/CloudsModel.h"
+#include "graphics/drawableObject.h"
+#include "graphics/VolumetricClouds.h"
+#include "graphics/Water.h"
+
+#include "gui/ImguiGUI.h"
+
+#include "platform/glError.h"
+#include "platform/Window.h"
+
+#include "utilities/utils.h"
+#include "utilities/constants.h"
+#include "utilities/terrain/Terrain.h"
+#include "utilities/texture/texture.h"
+
+#endif // CHIFENGINE
