cffe9350b9dd54050707e134b8e4fd30704adf10
Lukas Rennhofer <132039008+LukasRennhofer@users.noreply.github.com>
2025-04-03 20:55:23 +0200

    Updated TestFramework (TODO)

--- examples/TestFramework/main.cpp
+++ examples/TestFramework/main.cpp
@@ -14,4 +14,37 @@
 
 #include "../../src/CHIFEngine.h"
 
-// TODO !
+extern "C" int main(int argc, char **argv)
+ {
+	 glm::vec3 startPosition(0.0f, 800.0f, 0.0f);
+	 Camera camera(startPosition);
+ 
+	 int success;
+	 chif::Platform::Window window(success, 1920, 1800);
+	 if (!success)
+		 return -1;
+ 
+	 window.camera = &camera;
+ 
+	 chif::Graphics::Renderer::Buffer::FrameBufferObject SceneFBO(chif::Platform::Window::SCR_WIDTH, chif::Platform::Window::SCR_HEIGHT);
+	 glm::vec3 lightPosition, seed;
+	 glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)chif::Platform::Window::SCR_WIDTH / (float)chif::Platform::Window::SCR_HEIGHT, 5.f, 10000000.0f);
+	 // Font Test Loading
+	 shared_ptr<chif::GUI::Font> fontTest = shared_ptr<chif::GUI::Font>(new chif::GUI::Font("resources/calibri.ttf"));
+
+	 unique_ptr<chif::GUI::Label> label = unique_ptr<chif::GUI::Label>(new chif::GUI::Label(
+		fontTest,        
+		"Hello world!",
+		400,
+		400,
+		chif::Platform::Window::SCR_WIDTH,
+		chif::Platform::Window::SCR_WIDTH
+	  ));
+ 
+	 while (window.continueLoop())
+	 {
+		 // TODO
+ 
+		window.swapBuffersAndPollEvents();
+	 }
+ }
